14bb495c8525b5063a00b1575dbecd3bc979b973
Marijn Haverbeke <marijn@haverbeke.nl>
2021-06-25 19:05:00 +0200

    Adjust to new Lezer package names

--- src/lezer.ts
+++ src/lezer.ts
@@ -1,4 +1,4 @@
-import {parser} from "lezer-lezer"
+import {parser} from "@lezer/lezer"
 import {LezerLanguage, foldNodeProp, foldInside, LanguageSupport} from "@codemirror/language"
 import {styleTags, tags as t} from "@codemirror/highlight"
 
